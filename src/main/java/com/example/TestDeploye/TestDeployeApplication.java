a8b7c6d767ed8826900df5c626e77dafd2cd2ee8
fifalianajennyh <124344145+fifalianajennyh@users.noreply.github.com>
2024-01-26 20:37:09 +0300

    Initial commit

---
+++ src/main/java/com/example/TestDeploye/TestDeployeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestDeploye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestDeploye/TestDeployeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestDeploye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
